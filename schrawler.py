738e9a7833d351a1deba4f11b1f4cadfa5276494
Syvrus <btcorbita@gmail.com>
2021-06-08 11:55:38 -0700

    Create schrawler.py
    Implemented python  file intended for the crawler.
    
    Outfitted with proposed algorithm
    
    And some library to look for the url contents

---
+++ schrawler.py
@@ -0,0 +1,18 @@
+#used to get url contents apparently
+import urllib
+
+#Algorithm:
+#1.Get website url
+#2.Scan through website url
+#3.Get any hyperlinks not in the robot.txt and put in list
+#4.Store website content as txt file
+#5.Run txt file through Assignment 1 code
+#6.Repeat 4 and 5 for each link stored from 3
+#7.Do 6 for amount of levels time
+
+
+#Testing on one website at the moment
+print("Enter website to crawl: ")
+url = input()
+#might need to use urllib.request.urlopen()
+urllib.urlopen(url)
