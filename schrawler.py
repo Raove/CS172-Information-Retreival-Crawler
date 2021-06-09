8897bc058b16e5c877cc0bdb6288e0cbf4e68b9e
Brian Corbita <btcorbita@gmail.com>
2021-06-09 14:52:06 -0700

    changes
    altered code to follow instruction on one of the given websites.
    
    nothing outputted into file from tests

--- schrawler.py
+++ schrawler.py
@@ -1,11 +1,12 @@
 #used to get url contents apparently
 import requests
+from bs4 import BeautifulSoup
 
 #Algorithm:
 #1.Get website url
 #2.Scan through website url
 #3.Get any hyperlinks not in the robot.txt and put in list
-#4.Store website content as txt file
+#4.Store parse and store website content as txt file
 #5.Run txt file through Assignment 1 code
 #6.Repeat 4 and 5 for each link stored from 3
 #7.Do 6 for amount of levels time
@@ -14,9 +15,16 @@ import requests
 #Testing on one website at the moment
 print("Enter website to crawl: ")
 url = input()
+#gets the html code from the website
 output = requests.get(url)
+#use soup to get important content in site
+content = BeautifulSoup(output.text, "html.parser")
 
 #output
 file = open("output.txt", "a")
-file.write(output.text)
+for element in content.findAll('a', {'class':'s-access-detail-page'}):
+    title = element.get('title')
+    file.write(title)
+    words = element.get('href')
+    file.write(words)
 file.close()
