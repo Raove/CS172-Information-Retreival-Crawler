2be9e43cb1bc5b04f0d505d601c612d59e548e72
Raoul <6421655+Raove@users.noreply.github.com>
2021-06-10 20:15:45 -0700

    updates on crawler

--- crawler.py
+++ crawler.py
@@ -1,29 +1,63 @@
 import requests #requires pip install requests
 import re
-from urllib.parse import urlparse
 from bs4 import BeautifulSoup #requires pip install beautifulsoup4
+import urllib.robotparser
 
 exitProgram = False
-URLs = ["https://www.ucr.edu/", "https://www.ucla.edu/", "https://uci.edu/", "https://www.stanford.edu/"]
+URLs = ["https://www.ucla.edu/", "https://uci.edu/", "https://www.ucr.edu/", "https://www.stanford.edu/"]
 
 while(exitProgram != True):
     print("\n\nChoose an option:\n1. Run crawler and make HTML files.\n2. Build index.\n3. Show URL's crawled.\n4. Add URL to crawl.\n5. Exit program.\n")
     option = input()
     print('\n')
     if option == '1':
-        print("How many levels do you want to crawl?")
+        print("How many levels do you want to crawl per site?")
         levels = input()
-        source = requests.get('https://www.ucr.edu/').text
-        soup = BeautifulSoup(source, 'lxml')
-        f = open(r"C:\Users\raoul\Documents\GitHub\CS172-Information-Retreival-Crawler\files\page.html", "w")
-        f.write(soup.prettify())
-        f.close()
-        title = soup.find('title')
-        article = soup.find_all('article')
-        print(title.text)
-        # for arti in article:
-        #     print(arti.prettify())
-        
+        levels = int(levels)
+        for url in URLs:
+            rp = urllib.robotparser.RobotFileParser()
+            source = requests.get(url).text
+            pageUrls = []
+            level = 1
+            pageUrls.append(url)
+            rp.set_url(url + "robots.txt")
+            rp.read()
+            rp.crawl_delay('*')
+            while pageUrls and level < levels:
+                soup = BeautifulSoup(source, 'lxml')
+                filename = url.translate({ord(i): '' for i in ' |!@#$%^&*\()_+=<>\",.:;?/\\[]\{\}~`\n\t\r\b\f-\''})
+                filename = filename.replace('http', '')
+                f = open(r"C:\Users\raoul\Documents\GitHub\CS172-Information-Retreival-Crawler\files" +"\\"+ filename  + ".html", "w", encoding='utf-8')
+                f.write(soup.prettify())
+                f.close()
+                level = 1
+                for urls in soup.find_all('a', href=True):
+                    if urls not in pageUrls:
+                        # print(urls)
+                        if 'http' in urls.get('href'):
+                            if rp.can_fetch('*',urls.get('href')) == True:
+                                try:
+                                    source2 = requests.get(urls.get('href')).text
+                                except requests.exceptions.ConnectionError:
+                                    requests.status_codes = "Connection Refused"
+                                    print("Connection refused")
+                                soup2 = BeautifulSoup(source2, 'lxml')
+                                filename = urls.get('href').translate({ord(i): '' for i in ' |!@#$%^&*\()_+=<>\",.:;?/\\[]\{\}~`\n\t\r\b\f-\''})
+                                filename = filename.replace('http', '')
+                                f = open(r"C:\Users\raoul\Documents\GitHub\CS172-Information-Retreival-Crawler\files" +"\\"+ filename  + ".html", "w", encoding='utf-8')
+                                f.write(soup2.prettify())
+                                f.close()
+                                print(level)
+                                pageUrls.append(urls.get('href'))
+                                if level == levels:
+                                    break
+                                level += 1
+                            # print("We got here")
+                            else:
+                                print("Robots.txt denied this link")
+                                pageUrls.append(urls.get('href'))
+            # print(pageUrls)
+
     if option == '2':
         print("Good luck arturo")
     if option == '3':
