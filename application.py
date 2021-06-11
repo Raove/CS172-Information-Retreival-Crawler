c0db60c07d94a94248afc5e572238b599accdedb
choduh <choduh1995@gmail.com>
2021-06-11 15:35:45 -0700

    Webapp doko????

---
+++ application.py
@@ -0,0 +1,28 @@
+import flask
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+#from crawler import Query(?)
+
+@app.route('/')
+
+def home():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def home_post():
+    userQuery = request.form['search']
+
+    queryResult = Query.search(userQuery)
+    return render_template('WebApp.html', query=userQuery, resultList=queryResult)
+
+@app.route('/', methods=['POST'])
+def results():
+    userQuery = request.form['search']
+
+    queryResult = Query.search(userQuery)
+    return render_template('WebApp.html', query=userQuery, resultList=queryResult)
+
+if __name__ == '__main__':
+    app.run(debug = True)
